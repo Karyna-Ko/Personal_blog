c22cf75c78de819a975b3bbf59c994775664fbf7
Karyna-Ko <karina.korshak@gmail.com>
2022-05-27 15:57:18 +0300

    creation of a burger, adaptation of the first page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log('file 1');
+let burgerBtn = document.querySelector('.burger');
+let sideBar = document.querySelector('.sidebar');
+let pageDarken = document.querySelector('.page');
+burgerBtn.addEventListener('click', function(){
+	burgerBtn.classList.toggle('active');
+    sideBar.classList.toggle('active');
+    pageDarken.classList.toggle('active');
+});
